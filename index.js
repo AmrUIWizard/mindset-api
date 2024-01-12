e1b6c1b9a74cfa8ba14339b0968baae172797e58
3MooRa-0 <amr054327@gmail.com>
2024-01-13 01:11:46 +0200

    connect to mongo

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-import express, { request } from "express";
-import cors from "cors";
-import { Mongoose } from "mongoose";
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  `mongodb+srv://mongo:${process.env.DB_PASSWORD}@cluster0.3iafh6u.mongodb.net/?retryWrites=true&w=majority`
+);
+
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({
-    requestData: {
-      name,
-      email,
-      password,
-    },
-  });
+  const userDoc = await User.create({ name, email, password });
+  res.json(userDoc);
   // res.json("test ok");
 });
 app.post("/login", (req, res) => {
   res.json("test ok");
 });
-console.log("fdafsd");
+console.log(process.env.TESTING);
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,40 @@
-import { Mongoose } from "mongoose";
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const { Schema, model } = Mongoose;
-const UserSchema = new Schema({
-  name: { type: String, required: true, min: 4, unique: false },
-  email: { type: String, required: true, min: 4, unique: true },
-  password: { type: String, required: true },
+const { Schema, model } = mongoose;
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true, min: 4 },
+    email: { type: String, required: true, min: 4, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password before saving it to the database
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
-export const UserModel = model("User", UserSchema);
+// Compare the given password with the hashed password in the database
+UserSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const UserModel = model("User", UserSchema);
+
+module.exports = UserModel;
